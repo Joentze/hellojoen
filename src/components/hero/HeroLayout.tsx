299020d0dd25d141887c95967181ea29a2ba77b1
Tan Joen <tanjoen@Tans-MacBook-Pro.local>
2023-01-08 18:24:32 +0800

    feat: added notion character svg icon

--- src/components/hero/HeroLayout.tsx
+++ src/components/hero/HeroLayout.tsx
@@ -1,19 +1,30 @@
 import React from 'react'
+import { ReactComponent as HeroIcon } from '../HeroIcon.svg'
+interface IHeroLayout {
+  engageBtnFn?: () => void
+  connectBtnFn?: () => void
+}
 
-const HeroLayout = (): React.ReactElement => {
+const HeroLayout: React.FC<IHeroLayout> = ({ engageBtnFn, connectBtnFn }): React.ReactElement => {
   return <div className='w-full h-screen top-0 left-0 flex select-none'>
-    <div className="flex flex-col md:flex-row w-full h-fit">
-      <div className='text-left w-full p-10 pt-56 md:px-24 px-8'>
+    <div className="flex flex-col-reverse md:flex-row w-full h-fit">
+      <div className='text-left w-full p-10 md:pt-56 md:px-24 px-8'>
       <p className='text-[100px] font-bold text-slate-800'>Hello,</p>
       <p className='text-lg text-slate-600 line-clamp-4 -mt-2'>
       Welcome to my portfolio site, my name is Joen. Join me as I document my technical escapades and pen down my perspective of the world around me. I hope that you will find my adventures engaging.
       </p>
       <div className='flex flex-row w-full mt-10 gap-4 mb-20'>
-      <button className='ease-in duration-100 active:text-slate-300 active:bg-slate-700 shadow shadow-md hover:shadow-lg w-32 bg-slate-800 text-slate-100 h-12 font-bold rounded-lg'>Explore</button>
-      <button className='ease-in duration-100 active:text-slate-300 active:bg-slate-700 shadow shadow-md hover:shadow-lg w-32 border border-slate-800 text-slate-800 border-2 h-12 font-bold rounded-lg hover:bg-slate-800 hover:text-slate-100'>Connect</button>
+      <button
+        className='ease-in duration-100 active:text-slate-300 active:bg-slate-700 shadow shadow-md hover:shadow-lg w-32 bg-slate-800 text-slate-100 h-12 font-bold rounded-lg'
+        onClick={engageBtnFn}>Explore</button>
+      <button
+        className='ease-in duration-100 active:text-slate-300 active:bg-slate-700 shadow shadow-md hover:shadow-lg w-32 border border-slate-800 text-slate-800 border-2 h-12 font-bold rounded-lg hover:bg-slate-800 hover:text-slate-100'
+        onClick={connectBtnFn}
+      >Connect</button>
       </div>
       </div>
-      <div className='w-full bg-slate-800'>
+      <div className='w-full flex h-96 md:h-1/4'>
+        <div className='md:w-full md:h-full m-32 mt-32 md:mt-24'><HeroIcon/></div>
 
       </div>
     </div>
--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
-
+import { IoInformationCircleSharp, IoNewspaperSharp, IoChatbox } from 'react-icons/io5'
+import { ReactComponent as NotionCharIcon } from '../NotionCharIcon.svg'
 const NavBar = (): React.ReactElement => {
-  return <div className='bg-white border border-b-1 w-full h-20 top-0 left-0'></div>
+  return <div className='bg-white border border-b-1 w-full h-20 top-0 left-0 md:pl-20 flex flex-row p-4 gap-2'>
+    <button className='w-12 h-12 hover:bg-slate-200 ease-in duration-100 rounded-lg active:bg-slate-300'>
+        <NotionCharIcon/>
+    </button>
+    <div className='flex-grow h-fit flex flex-row gap-2'>
+        <div className='flex-grow'></div>
+    <button className='w-12 h-12 hover:bg-slate-200 ease-in duration-100 rounded-lg active:bg-slate-300'>
+        <IoNewspaperSharp className='m-auto text-slate-700 w-6 h-6'/>
+        </button>
+    <button className='w-12 h-12 hover:bg-slate-200 ease-in duration-100 rounded-lg active:bg-slate-300'>
+        <IoInformationCircleSharp className='m-auto text-slate-700 w-6 h-6'/>
+        </button>
+    <button className='w-12 h-12 hover:bg-slate-200 ease-in duration-100 rounded-lg active:bg-slate-300'>
+        <IoChatbox className='m-auto text-slate-700 w-6 h-6'/>
+        </button>
+    </div>
+  </div>
 }
 
 export default NavBar
