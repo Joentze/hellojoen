0cc7b65af16f003a71d9e28875cfee4ea74892ef
Joen Tan <joentze01@gmail.com>
2023-01-09 20:51:03 +0800

    feat: added link to home & projects page in nav bar

--- src/components/hero/HeroLayout.tsx
+++ src/components/hero/HeroLayout.tsx
@@ -9,19 +9,19 @@ const HeroLayout: React.FC<IHeroLayout> = ({ engageBtnFn, connectBtnFn }): React
   return <div className='w-full h-screen top-0 left-0 flex select-none bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-white via-slate-100 to-slate-400'>
     <div className="flex flex-col-reverse md:flex-row w-full h-fit">
       <div className='text-left w-full p-10 md:pt-56 md:px-24 px-8'>
-      <p className='text-[70px] md:text-[100px] font-bold text-slate-800'>Hello.</p>
-      <p className='text-lg text-slate-600 line-clamp-4 -mt-2'>
+        <p className='text-[70px] md:text-[100px] font-bold text-slate-800'>Hello.</p>
+        <p className='text-lg text-slate-600 line-clamp-4 -mt-2'>
       Welcome to my portfolio site, my name is <u className='decoration-4 hover:decoration-2 ease-in-out duration-200'>Joen.</u> Join me as I document my technical escapades and pen down my perspective of the world around me. I hope that you will find my adventures engaging.
-      </p>
-      <div className='flex flex-row w-full mt-10 gap-4 mb-20'>
-      <button
-        className='ease-in duration-100 active:text-slate-300 active:bg-slate-700 shadow shadow-md hover:shadow-lg w-32 bg-slate-800 text-slate-100 h-12 font-bold rounded-lg'
-        onClick={engageBtnFn}>Explore</button>
-      <button
-        className='ease-in duration-100 active:text-slate-300 active:bg-slate-700 shadow shadow-md hover:shadow-lg w-32 border border-slate-800 text-slate-800 border-2 h-12 font-bold rounded-lg hover:bg-slate-800 hover:text-slate-100'
-        onClick={connectBtnFn}
-      >Connect</button>
-      </div>
+        </p>
+        <div className='flex flex-row w-full mt-10 gap-4 mb-20'>
+          <button
+            className='ease-in duration-100 active:text-slate-300 active:bg-slate-700 shadow shadow-md hover:shadow-lg w-32 bg-slate-800 text-slate-100 h-12 font-bold rounded-lg'
+            onClick={engageBtnFn}>Explore</button>
+          <button
+            className='ease-in duration-100 active:text-slate-300 active:bg-slate-700 shadow shadow-md hover:shadow-lg w-32 border border-slate-800 text-slate-800 border-2 h-12 font-bold rounded-lg hover:bg-slate-800 hover:text-slate-100'
+            onClick={connectBtnFn}
+          >Connect</button>
+        </div>
       </div>
       <div className='w-full flex h-72 md:h-screen'>
         <div className='md:w-full md:h-full m-auto'><div className='scale-100 md:scale-75'><HeroIcon/></div></div>
--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -1,24 +1,36 @@
 import React from 'react'
-import { IoInformationCircleSharp, IoNewspaperSharp, IoChatbox } from 'react-icons/io5'
+import {
+  IoInformationCircleSharp,
+  IoNewspaperSharp,
+  IoChatbox
+} from 'react-icons/io5'
 import { ReactComponent as NotionCharIcon } from '../NotionCharIcon.svg'
+import { Link } from 'react-router-dom'
+
 const NavBar = (): React.ReactElement => {
-  return <div className='border border-b-1 w-full h-20 top-0 left-0 md:pl-20 flex flex-row p-4 gap-2'>
-    <button className='w-12 h-12 hover:bg-slate-200 ease-in duration-100 rounded-lg active:bg-slate-300'>
-        <NotionCharIcon/>
-    </button>
-    <div className='flex-grow h-fit flex flex-row gap-2'>
-        <div className='flex-grow'></div>
-    <button className='w-12 h-12 hover:bg-slate-200 ease-in duration-100 rounded-lg active:bg-slate-300'>
-        <IoNewspaperSharp className='m-auto text-slate-700 w-6 h-6'/>
+  return (
+    <div className="border border-b-1 w-full h-20 top-0 left-0 md:pl-20 flex flex-row p-4 gap-2">
+      <button className="w-12 h-12 hover:bg-slate-200 ease-in duration-100 rounded-lg active:bg-slate-300">
+        <Link to={'/'}>
+          <NotionCharIcon />
+        </Link>
+      </button>
+      <div className="flex-grow h-fit flex flex-row gap-2">
+        <div className="flex-grow"></div>
+        <button className="w-12 h-12 hover:bg-slate-200 ease-in duration-100 rounded-lg active:bg-slate-300">
+          <Link to={'/projects'}>
+            <IoNewspaperSharp className="m-auto text-slate-700 w-6 h-6" />
+          </Link>
         </button>
-    <button className='w-12 h-12 hover:bg-slate-200 ease-in duration-100 rounded-lg active:bg-slate-300'>
-        <IoInformationCircleSharp className='m-auto text-slate-700 w-6 h-6'/>
+        <button className="w-12 h-12 hover:bg-slate-200 ease-in duration-100 rounded-lg active:bg-slate-300">
+          <IoInformationCircleSharp className="m-auto text-slate-700 w-6 h-6" />
         </button>
-    <button className='w-12 h-12 hover:bg-slate-200 ease-in duration-100 rounded-lg active:bg-slate-300'>
-        <IoChatbox className='m-auto text-slate-700 w-6 h-6'/>
+        <button className="w-12 h-12 hover:bg-slate-200 ease-in duration-100 rounded-lg active:bg-slate-300">
+          <IoChatbox className="m-auto text-slate-700 w-6 h-6" />
         </button>
+      </div>
     </div>
-  </div>
+  )
 }
 
 export default NavBar
