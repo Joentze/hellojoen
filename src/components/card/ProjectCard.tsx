768dbdea448a082ad4761b66550aec07f64bf74d
Joen Tan <joentze01@gmail.com>
2023-01-14 17:19:00 +0800

    feat: changed opacity of project card

--- src/components/card/ProjectCard.tsx
+++ src/components/card/ProjectCard.tsx
@@ -14,13 +14,13 @@ const ProjectCard: React.FC<IProjectCard> = ({
   image
 }): React.ReactElement => {
   return (
-    <div className="w-full md:w-1/3 bg-white rounded-lg border border-slate-400 h-28 flex flex-row m-6 select-none overflow-hidden m-auto active:border-2">
+    <div className="shadow-lg w-full rounded-lg border-2 border-slate-500 backdrop-blur-lg bg-white/50  h-28 flex flex-row m-6 select-none overflow-hidden m-auto hover:bg-white ease-in duration-100 active:bg-slate-100">
       <div className='w-32 h-full bg-black'>
-          <img className='object-cover h-full' src={image}/>
+        <img className='object-cover h-full w-full' src={image}/>
       </div>
-      <div className='text-left w-full p-2 pl-4 h-max'>
-          <p className='font-bold text-xl text-slate-600'>{title}</p>
-          <p className='text-slate-500 line-clamp-2 justify'>{description}</p>
+      <div className='text-left w-full p-2 pl-4 h-full border-l-2 border-slate-500'>
+        <p className='font-bold text-xl text-slate-800'>{title}</p>
+        <p className='text-slate-600 line-clamp-2 justify'>{description}</p>
       </div>
     </div>
   )
