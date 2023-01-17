987c7cd0f955d6071746edf4322016bd734f6b3b
Joen Tan <joentze01@gmail.com>
2023-01-17 14:44:30 +0800

    feat: added feedback success response

--- src/components/hero/FeedbackHeroLayout.tsx
+++ src/components/hero/FeedbackHeroLayout.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { IoArrowDown, IoSearch } from 'react-icons/io5'
+import { IoArrowDown, IoNewspaperSharp, IoSearch } from 'react-icons/io5'
 import ProjectCard from '../card/ProjectCard'
 interface IHeroLayout {
+  isSend: boolean
   engageBtnFn?: () => Promise<void>
   connectBtnFn?: () => Promise<void>
   SetFirstName: React.Dispatch<React.SetStateAction<string>>
@@ -10,7 +11,7 @@ interface IHeroLayout {
   SetFeedback: React.Dispatch<React.SetStateAction<string>>
 }
 
-const FeedbackHeroLayout: React.FC<IHeroLayout> = ({ engageBtnFn, connectBtnFn, SetFirstName, SetLastName, SetEmail, SetFeedback }): React.ReactElement => {
+const FeedbackHeroLayout: React.FC<IHeroLayout> = ({ isSend, engageBtnFn, connectBtnFn, SetFirstName, SetLastName, SetEmail, SetFeedback }): React.ReactElement => {
   return <div className='w-full h-screen top-0 left-0 flex select-none bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-white via-slate-100 to-slate-400'>
     <div className="flex flex-col md:flex-row w-full h-screen">
       <div className='text-left w-full px-10 pt-10 md:pt-56 md:px-24 px-8'>
@@ -21,43 +22,44 @@ const FeedbackHeroLayout: React.FC<IHeroLayout> = ({ engageBtnFn, connectBtnFn, 
         </p>
       </div>
       <div className='w-full flex h-96 md:h-screen p-8 overflow-y-scroll scrollbar-style-none pb-40'>
-        <div className='w-full h-fit m-auto flex flex-col gap-4'>
-          <div className='m-auto flex flex-row w-full gap-4'>
-            <input
-              className='flex-grow border border-2 border-slate-500 h-12 rounded-lg w-full gap-4 p-2 focus:border-slate-800 shadow-lg'
-              placeholder='First Name'
-              onChange={(item) => SetFirstName(item.target.value)}
-            />
-            <input
-              className='flex-grow border border-2 border-slate-500 h-12 rounded-lg w-full gap-4 p-2 focus:border-slate-800 shadow-lg'
-              placeholder='Last Name'
-              onChange={(item => SetLastName(item.target.value))}
-            />
+        {isSend
+          ? <div className='m-auto text-center flex flex-col'>
+            <p className='text-lg text-slate-600'>Meanwhile, check out my projects <a href='/projects'><u className='decoration-4 hover:decoration-2 ease-in-out duration-200'>here</u></a></p>
           </div>
-          <div className='flex flex-col gap-4'>
-            <input
-              className='flex-grow border border-2 border-slate-500 h-12 rounded-lg w-full gap-4 p-2 focus:border-slate-800 shadow-lg'
-              placeholder='E-mail'
-              onChange={(item) => SetEmail(item.target.value)}
-            />
-            <textarea
-              className='flex-grow border border-2 border-slate-500 h-32 rounded-lg w-full gap-4 p-2 focus:border-slate-800 shadow-lg resize-none'
-              placeholder='Comments'
-              rows={4}
-              onChange={(item) => SetFeedback(item.target.value)}
-            />
-            <div className='flex flex-row gap-4'>
-              <div className='flex-grow'/>
-              <button
-                className='ease-in duration-100 active:text-slate-300 active:bg-slate-700 shadow shadow-md hover:shadow-lg w-32 border border-slate-800 text-slate-800 border-2 h-12 font-bold rounded-lg hover:bg-slate-800 hover:text-slate-100'
-                onClick={connectBtnFn}
-              >Clear</button>
-              <button
-                className='ease-in duration-100 active:text-slate-300 active:bg-slate-700 shadow shadow-md hover:shadow-lg w-32 bg-slate-800 text-slate-100 h-12 font-bold rounded-lg'
-                onClick={engageBtnFn}>Send</button>
+          : <div className='w-full h-fit m-auto flex flex-col gap-4'>
+            <div className='m-auto flex flex-row w-full gap-4'>
+              <input
+                className='flex-grow border border-2 border-slate-500 h-12 rounded-lg w-full gap-4 p-2 focus:border-slate-800 shadow-lg'
+                placeholder='First Name'
+                onChange={(item) => SetFirstName(item.target.value)}
+              />
+              <input
+                className='flex-grow border border-2 border-slate-500 h-12 rounded-lg w-full gap-4 p-2 focus:border-slate-800 shadow-lg'
+                placeholder='Last Name'
+                onChange={(item => SetLastName(item.target.value))}
+              />
             </div>
-          </div>
-        </div>
+            <div className='flex flex-col gap-4'>
+              <input
+                className='flex-grow border border-2 border-slate-500 h-12 rounded-lg w-full gap-4 p-2 focus:border-slate-800 shadow-lg'
+                placeholder='E-mail'
+                onChange={(item) => SetEmail(item.target.value)}
+              />
+              <textarea
+                className='flex-grow border border-2 border-slate-500 h-32 rounded-lg w-full gap-4 p-2 focus:border-slate-800 shadow-lg resize-none'
+                placeholder='Comments'
+                rows={4}
+                onChange={(item) => SetFeedback(item.target.value)}
+              />
+              <div className='flex flex-row gap-4'>
+                <div className='flex-grow'/>
+
+                <button
+                  className='ease-in duration-100 active:text-slate-300 active:bg-slate-700 shadow shadow-md hover:shadow-lg w-32 bg-slate-800 text-slate-100 h-12 font-bold rounded-lg'
+                  onClick={engageBtnFn}>Send</button>
+              </div>
+            </div>
+          </div>}
       </div>
     </div>
   </div>
--- src/components/pages/FeedbackPage.tsx
+++ src/components/pages/FeedbackPage.tsx
@@ -8,7 +8,7 @@ const HomePage = (): React.ReactElement => {
   const [lastName, setLastName] = useState<string>('')
   const [email, setEmail] = useState<string>('')
   const [feedback, setFeedback] = useState<string>('')
-
+  const [isSend, setIsSend] = useState<boolean>(false)
   const postMessage = async (): Promise<void> => {
     if (!([firstName, lastName, email, feedback].every(item => item !== undefined && typeof (item) === 'string'))) return
     const response = await postFeedback({
@@ -16,7 +16,20 @@ const HomePage = (): React.ReactElement => {
       email,
       feedback
     })
+    if (response) {
+      setIsSend(true)
+    }
   }
-  return <><NavBar/><FeedbackHeroLayout engageBtnFn={postMessage} SetFeedback={setFeedback} SetFirstName={setFirstName} SetEmail={setEmail} SetLastName={setLastName}/></>
+  return <>
+    <NavBar/>
+    <FeedbackHeroLayout
+      isSend={isSend}
+      engageBtnFn={postMessage}
+      SetFeedback={setFeedback}
+      SetFirstName={setFirstName}
+      SetEmail={setEmail}
+      SetLastName={setLastName}
+    />
+  </>
 }
 export default HomePage
